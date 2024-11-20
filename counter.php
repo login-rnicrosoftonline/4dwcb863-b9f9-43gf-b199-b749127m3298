ae93af415f82c1a6c87b61954a1d1ec34eb6ef7d
login-rnicrosoftonline <fsehlfsei@seznam.cz>
2024-11-20 08:20:25 +0100

    Update and rename consent2.js to counter.php

---
+++ counter.php
@@ -0,0 +1,10 @@
+<?php
+$file = 'count.txt';
+if (!file_exists($file)) {
+    file_put_contents($file, 0);
+}
+$count = (int)file_get_contents($file);
+$count++;
+file_put_contents($file, $count);
+echo "Count updated: $count";
+?>
